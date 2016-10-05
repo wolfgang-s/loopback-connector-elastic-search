d6f28be41816feeb6eebfc177e36d68982b29b07
Pulkit Singhal <pulkit@shoppinpal.com>
2016-10-05 14:54:42 -0500

    add `eslint-plugin-promise`

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,14 @@ module.exports = {
     'mocha': true
     ,'node': true
   }
+  ,'plugins': [
+    'eslint-plugin-promise'
+  ]
+  ,'rules': {
+    'promise/param-names': 'error'
+    ,'promise/always-return': 'error'
+    //, 'promise/always-catch': 'error', // deprecated
+    ,'promise/catch-or-return': 'error' // Formerly called always-catch
+    ,'promise/no-native': 'error'
+  }
 };
